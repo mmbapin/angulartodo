7c4e393301ddeb7d8fcddea7b7c37d8726581fa5
mmbapin <bapinmm@gmail.com>
2023-04-11 02:57:01 +0600

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { TodoComponent } from './todo/todo.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    TodoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
