f30f198b951d0e36d0bd41f143cb877429ff8759
M. M. Bapin <m.bapin@konasl.com>
2023-04-11 14:27:31 +0600

    firestore added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
 import { CategoryComponent } from './category/category.component';
 import { TodoComponent } from './todo/todo.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore"
 
 @NgModule({
   declarations: [
@@ -18,9 +31,23 @@ import { FooterComponent } from './layout/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  firebase: {
+    projectId: 'todo-3ee53',
+    appId: '1:912169165130:web:e8be9ebc09d63dc6b661b3',
+    databaseURL: 'https://todo-3ee53-default-rtdb.firebaseio.com',
+    storageBucket: 'todo-3ee53.appspot.com',
+    apiKey: 'AIzaSyCcP0YC9Q6nhjCV7SBycTb4IURM7Py952A',
+    authDomain: 'todo-3ee53.firebaseapp.com',
+    messagingSenderId: '912169165130',
+    measurementId: 'G-2ZHG4W8SLN',
+  },
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCcP0YC9Q6nhjCV7SBycTb4IURM7Py952A",
+    authDomain: "todo-3ee53.firebaseapp.com",
+    databaseURL: "https://todo-3ee53-default-rtdb.firebaseio.com",
+    projectId: "todo-3ee53",
+    storageBucket: "todo-3ee53.appspot.com",
+    messagingSenderId: "912169165130",
+    appId: "1:912169165130:web:e8be9ebc09d63dc6b661b3",
+    measurementId: "G-2ZHG4W8SLN"
+  }
+}
