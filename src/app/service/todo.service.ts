e5783254c39c950e7bd6a9e79a3f5dadd404b19f
mmbapin <bapinmm@gmail.com>
2023-04-14 04:21:33 +0600

    Code added

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FieldValue } from 'firebase/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { map, retry } from 'rxjs/operators';
+import { increment } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  constructor(private _afs: AngularFirestore, private toastr: ToastrService) {}
 
-  constructor() { }
+  saveTodo(id: string, data: any) {
+    // console.log(Firestore);
+    this._afs
+      .collection('categories')
+      .doc(id)
+      .collection('todos')
+      .add(data)
+      .then((ref) => {
+        this._afs.doc('categories/' + id).update({ todoCount: increment(1) });
+        this.toastr.success('New Todo Saved Successfully.');
+      });
+  }
+
+  loadTodos(id: string) {
+    return this._afs
+      .collection('categories')
+      .doc(id)
+      .collection('todos')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((todo) => {
+            const data = todo.payload.doc.data();
+            const id = todo.payload.doc.id;
+            return { id, data };
+          });
+        })
+      );
+  }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TodoService } from '../service/todo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  todoText: string = '';
+  buttonName: string = 'Add';
+  categoryId: any = '';
+  todoList: Array<any> = [];
+  constructor(
+    private todoService: TodoService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.categoryId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.todoService.loadTodos(this.categoryId).subscribe((val) => {
+      console.log(val);
+      this.todoList = val;
+    });
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f);
+    let todo = {
+      todo: f.value.todoText,
+      isCompleted: false,
+    };
+    this.todoService.saveTodo(this.categoryId, todo);
+    f.resetForm();
+  }
 }
